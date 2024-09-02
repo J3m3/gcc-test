1eb35748c2e560a3b281c792c7a78cb5df8dde62
Jesung Yang <y.j3ms.n@gmail.com>
2024-09-02 11:01:21 +0900

    backup

---
+++ src/run-Oz.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-Oz-cpugcc_r-502.sh
+
+./asm_gen_non-mod-Oz.sh
+./asm_gen_mod-Oz.sh
+
+./compare_s.sh > result-Oz
---
+++ src/run.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-cpugcc_r-502.sh
+
+./asm_gen_non-mod.sh
+./asm_gen_mod.sh
+
+./compare_s.sh > result-O3
